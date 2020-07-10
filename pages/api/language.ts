09c19735db2efe7af48fc4802dc38c93a80ebe9b
Jean Pasqualini <jpasqualini@wynd.eu>
2020-07-10 14:54:57 +0200

    migrating to typescript

--- pages/api/language.ts
+++ pages/api/language.ts
@@ -1,6 +1,7 @@
 import languageSet from "../../config/language_set";
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req, res) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify(languageSet))
